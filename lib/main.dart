286c88d4ed7c96a5fb521a7521336f9681ee4d82
Ervan Chodry <ervanchodry112@gmail.com>
2022-09-13 11:04:03 +0700

    LKP 2 Mobile Lanjut

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
+import 'home.dart';
+
+void main() {
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({Key? key}) : super(key: key);
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final theme = FooderlichTheme.light();
+    return MaterialApp(
+      title: 'Fooderlich',
+      theme: theme,
+      home: const Home(),
+    );
+  }
+}
