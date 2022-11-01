bf4b486a96c080aa48affe447900995166423921
Ervan Chodry <ervanchodry112@gmail.com>
2022-11-01 08:36:07 +0700

    Update dari project lama

---
+++ lib/navigation/app_router.dart
@@ -0,0 +1,108 @@
+import 'package:fooderlich/models/models.dart';
+
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../models/models.dart';
+import '../screens/screens.dart';
+
+class AppRouter {
+  final AppStateManager appStateManager;
+  final ProfileManager profileManager;
+  final GroceryManager groceryManager;
+
+  AppRouter(this.appStateManager, this.profileManager, this.groceryManager);
+
+  late final router = GoRouter(
+      debugLogDiagnostics: true,
+      refreshListenable: appStateManager,
+      initialLocation: '/login',
+      routes: [
+        GoRoute(
+          name: 'login',
+          path: '/login',
+          builder: (BuildContext context, GoRouterState state) =>
+              const LoginScreen(),
+        ),
+        GoRoute(
+          name: 'onboarding',
+          path: '/onboarding',
+          builder: (BuildContext context, GoRouterState state) =>
+              const OnboardingScreen(),
+        ),
+        GoRoute(
+          name: 'home',
+          path: '/:tab',
+          builder: (BuildContext context, GoRouterState state) {
+            final tab = int.tryParse(state.params['tab'] ?? '') ?? 0;
+            return Home(key: state.pageKey, currentTab: tab);
+          },
+          routes: [
+            GoRoute(
+              name: 'item',
+              path: 'item/:id',
+              builder: (BuildContext context, GoRouterState state) {
+                final itemId = state.params['id'] ?? '';
+                final item = groceryManager.getGroceryItem(itemId);
+                return GroceryItemScreen(
+                  originalItem: item,
+                  onCreate: (item) {
+                    groceryManager.addItem(item);
+                  },
+                  onUpdate: (item) {
+                    groceryManager.updateItem(item);
+                  },
+                );
+              },
+            ),
+            GoRoute(
+                name: 'profile',
+                path: 'profile',
+                builder: (BuildContext context, GoRouterState state) {
+                  final tab = int.tryParse(state.params['tab'] ?? '') ?? 0;
+                  return ProfileScreen(
+                    user: profileManager.getUser,
+                    currentTab: tab,
+                  );
+                },
+                routes: [
+                  GoRoute(
+                    name: 'rw',
+                    path: 'rw',
+                    builder: (BuildContext context, GoRouterState state) =>
+                        const WebViewScreen(),
+                  ),
+                ]),
+          ],
+        ),
+      ],
+      redirect: (state) {
+        final loggedIn = appStateManager.isLoggedIn;
+        final loggingIn = state.subloc == '/login';
+        if (!loggedIn) {
+          return loggingIn ? null : '/login';
+        }
+
+        final isOnBoardingComplete = appStateManager.isOnboardingComplete;
+        final onboarding = state.subloc == '/onboarding';
+        if (!isOnBoardingComplete) {
+          return onboarding ? null : '/onboarding';
+        }
+
+        if (loggingIn || onboarding) {
+          return '/${FooderlichTab.explore}';
+        }
+        return null;
+      },
+      errorPageBuilder: (BuildContext context, GoRouterState state) {
+        return MaterialPage(
+          key: state.pageKey,
+          child: Scaffold(
+            body: Center(
+              child: Text(
+                state.error.toString(),
+              ),
+            ),
+          ),
+        );
+      });
+}
---
+++ lib/screens/empty_grocery_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+
+import '../models/app_state_manager.dart';
+
+class EmptyGroceryScreen extends StatelessWidget {
+  const EmptyGroceryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(30.0),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Flexible(
+              child: AspectRatio(
+                aspectRatio: 1 / 1,
+                child: Image.asset('assets/fooderlich_assets/empty_list.png'),
+              ),
+            ),
+            Text(
+              'No Groceries',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            const SizedBox(height: 16.0),
+            const Text(
+              'Shopping for ingredients?\n'
+              'Tap the + button to write them down!',
+              textAlign: TextAlign.center,
+            ),
+            MaterialButton(
+              textColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              color: Colors.green,
+              onPressed: () {
+                GoRouter.of(context).goNamed(
+                  'home',
+                  params: {
+                    'tab': '${FooderlichTab.recipes}',
+                  },
+                );
+                Provider.of<AppStateManager>(context, listen: false)
+                    .goToRecipes();
+              },
+              child: const Text('Browse Recipes'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
