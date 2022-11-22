51bb8d56e7e83d428b60edfc6306e451fee46e78
Ervan Chodry <ervanchodry112@gmail.com>
2022-11-22 09:15:54 +0700

    Add API

--- lib/api/get_recipes.dart
+++ lib/api/get_recipes.dart
@@ -5,15 +5,16 @@ import 'package:http/http.dart' as http;
 class GetRecipes {
   static var URL = Uri.https('uji.fmipa.unila.ac.id', '/api-test/recipes.json');
 
-  static Future getRecipes() async {
+  static Future<List<SimpleRecipe>> getRecipes() async {
     final response = await http.get(URL);
     if (response.statusCode == 200) {
       final Map<String, dynamic> json = convert.jsonDecode(response.body);
+      print(response.body);
 
       if (json['recipes'] != null) {
-        final recipes = <ExploreRecipe>[];
+        final recipes = <SimpleRecipe>[];
         json['recipes'].foreach((v) {
-          recipes.add(ExploreRecipe.fromJson(v));
+          recipes.add(SimpleRecipe.fromJson(v));
         });
         print(recipes[3].title);
         return recipes;
@@ -21,7 +22,8 @@ class GetRecipes {
         return [];
       }
     }
+    return [];
 
-    return response;
+    // return response;
   }
 }
--- lib/screens/recipes_screen.dart
+++ lib/screens/recipes_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fooderlich/api/get_recipes.dart';
 
 import '../api/mock_fooderlich_service.dart';
 import '../components/components.dart';
@@ -12,7 +13,8 @@ class RecipesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: exploreService.getRecipes(),
+      // future: exploreService.getRecipes(),
+      future: GetRecipes.getRecipes(),
       builder: (context, AsyncSnapshot<List<SimpleRecipe>> snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
           return RecipesGridView(recipes: snapshot.data ?? []);
