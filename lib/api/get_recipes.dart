d6cbf1e6cc5dbcce5ed47d2e649d031829538f95
Ervan Chodry <ervanchodry112@gmail.com>
2022-11-24 13:25:28 +0700

    Add Networking API with DIO and HTTP Library

--- lib/api/get_recipes.dart
+++ lib/api/get_recipes.dart
@@ -1,29 +1,38 @@
 import 'dart:convert' as convert;
 import 'package:fooderlich/models/models.dart';
 import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
 
 class GetRecipes {
   static var URL = Uri.https('uji.fmipa.unila.ac.id', '/api-test/recipes.json');
+  static var dio = Dio();
 
   static Future<List<SimpleRecipe>> getRecipes() async {
-    final response = await http.get(URL);
+    // final response = await http.get(URL);
+    final response = await dio.get('https://uji.fmipa.unila.ac.id/api-test/recipes.json');
     if (response.statusCode == 200) {
-      final Map<String, dynamic> json = convert.jsonDecode(response.body);
-      print(response.body);
+      final Map<String, dynamic> json = convert.jsonDecode(response.toString());
+      // final Map<String, dynamic> json = convert.jsonDecode(response.body);
+      // print(response.body);
 
       if (json['recipes'] != null) {
         final recipes = <SimpleRecipe>[];
-        json['recipes'].foreach((v) {
+        json['recipes'].forEach((v) {         
+          print(v);
           recipes.add(SimpleRecipe.fromJson(v));
         });
-        print(recipes[3].title);
+        // print(recipes[3].title);
         return recipes;
-      } else {
+      }else{
         return [];
       }
     }
+
     return [];
+    
 
     // return response;
   }
+
+
 }
